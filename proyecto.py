8504f7cbb2db5da9be3ff461d5085e2eb9cc03da
Alejandro <alegomezse@unal.edu.co>
2021-06-29 13:26:43 -0500

    se cambio el orden de las constants, se definieron las constantes iniciales, se corrigío el uso de y por g en la definición de constantes y se realizo la primera iteración

--- proyecto.py
+++ proyecto.py
@@ -4,6 +4,7 @@ import math as m
 g = 9.8#m/s**2
 m1 = 2.0#kg
 m2 = 3.0#kg
+t = 0
 lon1 = 4.0#cm
 lon2 = 2.0#cm
 #VARIABLES INICIALES
@@ -44,38 +45,41 @@ def o(dt,x1,x2,x3,x4):
     oRespuesta = oArriba/oAbajo
     return oRespuesta
 
-
+#valores de la función iniciales
+a = l(t,x1,x2,x3,x4)
+b = mm(t,x1,x2,x3,x4)
+y = n(t,x1,x2,x3,x4)
+g = o(t,x1,x2,x3,x4)
 #RK4 SEGÚN EL PROFE
-def rk4(N,a,b,y,g):
-    t = 0
+def rk4(N,a,b,y,g):  
     dt = 0.2
     for i in range(N):
         l1 = l(t,a,b,y,g)
-        l2 = l(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
-        l3 = l(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        l4 = l(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),y +((o3*dt)))
-
         mm1 = mm(t,a,b,y,g)
-        mm2 = mm(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
-        mm3 = mm(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        mm4 = mm(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),y +((o3*dt)))
-
         n1 = n(t,a,b,y,g)
-        n2 = n(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
-        n3 = n(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        n4 = n(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),y +((o3*dt)))
-        
         o1 = o(t,a,b,y,g)
-        o2 = o(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
-        o3 = o(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        o4 = o(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),y +((o3*dt)))
+
+        l2 = l(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),g +((o1*dt)/2))
+        mm2 = mm(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),g +((o1*dt)/2))
+        n2 = n(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),g +((o1*dt)/2))
+        o2 = o(t + dt/2,a+((dt*l1)/2),b +((mm1*dt)/2),y +((n1*dt)/2),g +((o1*dt)/2))
+
+        l3 = l(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),g +((o2*dt)/2))
+        mm3 = mm(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),g +((o2*dt)/2))
+        n3 = n(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),g +((o2*dt)/2))
+        o3 = o(t + dt/2,a+((dt*l2)/2),b +((mm2*dt)/2),y +((n2*dt)/2),g +((o2*dt)/2))
+
+        l4 = l(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),g +((o3*dt)))
+        mm4 = mm(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),g +((o3*dt)))
+        n4 = n(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),g +((o3*dt)))
+        o4 = o(t + dt,a+((dt*l3)),b +((mm3*dt)),y +((n3*dt)),g +((o3*dt)))
 
         a += dt/6*(l1+2*l2+2*l3+l4)
         b += dt/6*(mm1+2*mm2+2*mm3+mm4)
         y += dt/6*(n1+2*n2+2*n3+n4)
         g += dt/6*(o1+2*o2+2*o3+o4)
-
-
+        print(a," ",b," ",y," ",g)
+rk4(10, a,b,y,g)
         
 
     
