97e7cfe8a104cde512086859c33624a38fdfe2b0
alegomezse <79580651+alegomezse@users.noreply.github.com>
2021-06-29 11:59:06 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -40,22 +40,22 @@ def rk4(N,a,b,y,g):
         l1 = l(t,a,b,y,g)
         l2 = l(t + dt/2,a+((dt*l1)/2),b +((m1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
         l3 = l(t + dt/2,a+((dt*l2)/2),b +((m2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        l4 = l(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)/2))
+        l4 = l(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)))
 
         m1 = mm(t,a,b,y,g)
         m2 = mm(t + dt/2,a+((dt*l1)/2),b +((m1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
         m3 = mm(t + dt/2,a+((dt*l2)/2),b +((m2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        m4 = mm(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)/2))
+        m4 = mm(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)))
 
         n1 = n(t,a,b,y,g)
         n2 = n(t + dt/2,a+((dt*l1)/2),b +((m1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
         n3 = n(t + dt/2,a+((dt*l2)/2),b +((m2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        n4 = n(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)/2))
+        n4 = n(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)))
         
         o1 = o(t,a,b,y,g)
         o2 = o(t + dt/2,a+((dt*l1)/2),b +((m1*dt)/2),y +((n1*dt)/2),y +((o1*dt)/2))
         o3 = o(t + dt/2,a+((dt*l2)/2),b +((m2*dt)/2),y +((n2*dt)/2),y +((o2*dt)/2))
-        o4 = o(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)/2))
+        o4 = o(t + dt/2,a+((dt*l3)/2),b +((m3*dt)/2),y +((n3*dt)/2),y +((o3*dt)))
 
         a += dt/6*(l1+2*l2+2*l3+l4)
         b += dt/6*(m1+2*m2+2*m3+m4)
